655055ccf8fad8855bb8bf455e5c2bc75de3272e
Astrid Krickl <akrickl@student.tgm.ac.at>
2014-12-22 12:17:40 +0100

    verbessern und kommentieren von factory pattern

--- factory/Fabrik.py
+++ factory/Fabrik.py
@@ -8,26 +8,44 @@ class Fabrik:
     lager = None #artikel array
     @abstractmethod
     def verkaufen(self):
+        """
+        verkauft aritkel
+        """
         pass
 
     @abstractmethod
     def verpacken(self):
+        """
+        verpackt artikel
+        """
         pass
 
     @abstractmethod
     def verschicken(self):
+        """
+        verschickt artikel
+        """
         pass
 
     @classmethod
     def erzeugen(cls, type):
-        if type(type) == Schuhe:
-            return Schuhe
-        elif type(type) == Computer:
-            return Computer
+        """
+        erzeugt artikel vom angegeben typ
+        :param type: artikelart die produziert werden soll
+        :return: erzuegter typ
+        """
+
+        Fabrik.getFabrik(type).erzeugen(type)
+        return type
 
     @classmethod
     def getFabrik(cls, type):
-        if type(type) == Schuhfabrik:
+        """
+        gibt die fabrik zurück die verwendet werden soll
+        :param type: vom typen artikel der erzeugt werden soll
+        :return: fabrik die verwendet werden soll
+        """
+        if type(type) == Schuhe:
             return Schuhfabrik
-        elif type(type) == Computerfabrik:
+        elif type(type) == Computer:
             return Computerfabrik
